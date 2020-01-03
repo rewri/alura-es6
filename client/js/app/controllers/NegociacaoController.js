a317760c0e10210b5a8b290d3faa2b7b3399a5dd
Rérisson Fumes <rkfumes@gmail.com>
2020-01-03 16:37:47 -0300

    melhorias no controller

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,25 @@
+class NegociacaoController {
+
+    constructor() {
+
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
+    }
+
+    adiciona(event) {
+
+        event.preventDefault();
+
+        let negociacao = new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
+
+    }
+}
--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,12 +1,11 @@
 class Negociacao {
 
-    construtor(data, quantidade, valor) {
+    constructor(data, quantidade, valor) {
 
         this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
         Object.freeze(this);
-
     }
 
     get volume() {
